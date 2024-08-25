7f55ee9989c3998d1f4f45fc0728a516e6f37d06
maciejwitwicki <maciej.ygrek@gmail.com>
2024-08-25 10:11:43 +0200

    added FLAG_KEEP_SCREEN_ON to disable screensaver

--- app/src/main/java/com/mwi/oledsaver/MainActivity.kt
+++ app/src/main/java/com/mwi/oledsaver/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.StrictMode
 import android.os.StrictMode.VmPolicy
 import android.util.Log
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.mwi.oledsaver.config.ConfigProvider
 
@@ -22,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         if (CONFIG.isEnabled()) {
             Log.i(TAG, "Created MainActivity")
             setContentView(R.layout.activity_main)
+            window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         } else {
             Log.i(TAG, "Out of operating hours, finishing")
             finishAndRemoveTask()
